02863da27349328a6c9002486efc465af4d6eed7
EllisBale <elscar29@gmail.com>
2025-07-12 19:14:43 +0100

    Add code for deployment

--- italiano_table/settings.py
+++ italiano_table/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5d%i+xl0z=wd63&h+_xm34ihad+q5))v@eqy+7dukzhuhxu#i+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                '127.0.0.1',]
 
 
 # Application definition
